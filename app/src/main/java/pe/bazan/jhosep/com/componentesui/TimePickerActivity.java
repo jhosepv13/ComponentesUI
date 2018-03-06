5e1db77e095a87dc6fdd791fe965384a307028da
Jhosep Bazan <jhosep.bazan@tecsup.edu.pe>
2018-03-06 12:40:59 -0500

    Subiendo avances

---
+++ app/src/main/java/pe/bazan/jhosep/com/componentesui/TimePickerActivity.java
@@ -0,0 +1,13 @@
+package pe.bazan.jhosep.com.componentesui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_picker);
+    }
+}
