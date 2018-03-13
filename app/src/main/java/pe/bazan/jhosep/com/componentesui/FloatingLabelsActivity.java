105cea909b8cdd99b1d336ba323635fe8b45ba01
Jhosep Bazan <jhosep.bazan@tecsup.edu.pe>
2018-03-13 00:19:54 -0500

    Subiendo segundo avance de Laboratorio

---
+++ app/src/main/java/pe/bazan/jhosep/com/componentesui/FloatingLabelsActivity.java
@@ -0,0 +1,13 @@
+package pe.bazan.jhosep.com.componentesui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FloatingLabelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_labels);
+    }
+}
